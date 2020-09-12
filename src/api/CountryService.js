a3bb983d2822115a866c041fb89069c5908b8364
geancarlocordova <geancarlo.cordova@kireygroup.com>
2020-09-12 16:17:40 +0200

    await missed in response countries

--- src/api/CountryService.js
+++ src/api/CountryService.js
@@ -28,7 +28,7 @@ router.get('/', async (req, res) => {
 
 
 router.get('/all', async (req, res) => {
-  const entities = Entity.find();
+  const entities = await Entity.find();
   res.json(entities);
 });
 
